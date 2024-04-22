e21cea3f14ad4fc3ed0b37589679d3978aebcb33
Anthony4m <amoahanthony41@gmail.com>
2024-04-22 19:28:58 +0000

    created a enail messsage

---
+++ resources/views/emails/task_pdf.blade.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tasks Summary</title>
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+        }
+        .signature {
+            font-weight: bold;
+            font-size: larger;
+        }
+    </style>
+</head>
+<body>
+<p>Good morning,</p>
+<p>I hope you had a nice weekend.</p>
+<p>Please find attached your end of week summary for last week.</p>
+<p>Have a great week ahead.</p>
+<p>Kind regards</p>
+<p><span class="signature">Bernadette Bawuah</span><br>
+    <span class="signature" style="font-size: larger;">Business Administrative Manager</span><br>
+    Tel: 0333 345 5486<br>
+    Mob: 07780 874704</p>
+</body>
+</html>
